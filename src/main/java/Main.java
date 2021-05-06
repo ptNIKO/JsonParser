2f7521e69351a93a5c1b430ac6f96790770fdd3c
ptNIKO <simonich2016@gmail.com>
2021-05-06 15:41:49 +0300

    test commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         LogService logService = new LogService();
         ConsoleView consoleView = new ConsoleView();
         Parser parser = new Parser();
